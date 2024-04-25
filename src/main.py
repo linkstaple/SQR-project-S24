1c026c6114969202c0ef22b35a370199b94e11fc
Michael Dmitriev <m.dmitriev@innopolis.university>
2024-04-25 22:44:30 +0300

    easter egg

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
@@ -8,6 +10,8 @@ if __name__ == "__main__":
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="static")
 
 @app.get("/")
 async def root():
@@ -48,6 +52,8 @@ async def group_info(id: str):
 async def group():
     return {"message": "Hello World"}
 
-
-if __name__ == "__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+@app.get("/easter_egg", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
