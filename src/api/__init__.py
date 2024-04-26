b22e998c2cb130c96e3469cf86fe4a0218364306
5ur3 <an.sandimirov@gmail.com>
2024-04-26 23:33:14 +0300

    Reformat source files

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,38 +2,33 @@ from model import RegisterUser, LoginUser
 import service.user
 import service.group
 
+
 def setup(app):
     @app.post("/api/register")
     async def register(user: RegisterUser):
         return await service.user.register(user)
 
-
     @app.post("/api/login")
     async def login(user: LoginUser):
         return await service.user.login(user)
-
 
     @app.get("/api/profile")
     async def profile():
         return {"message": "Hello World"}
 
-
     @app.get("/api/groups")
     async def groups():
-        return await service.group.get_users(1) #todo extract user_id from request
-
+        # todo extract user_id from request
+        return await service.group.get_users(1)
 
     @app.post("/api/group")
     async def group():
         return {"message": "Hello World"}
 
-
     @app.get("/api/group/{id}")
     async def group_info(id: str):
         return {"message": "Hello World"}
 
-
     @app.post("/api/split")
     async def group():
         return {"message": "Hello World"}
-
--- src/db/user.py
+++ src/db/user.py
@@ -1,13 +1,14 @@
 import model
 from .sqlite import Database
 
-class _User():
+
+class _User:
     def list_users(self):
         return Database.query("select * from users").fetchall()
 
     def register_user(self, username, hashed_password):
         Database.execute("insert into users (username, password) values ($1, $2)",
-                    username, hashed_password).fetchall()
+                         username, hashed_password).fetchall()
 
     def get_user_by_credentials(self, username, hashed_password) -> model.User:
         resp = (Database.
@@ -18,5 +19,6 @@ class _User():
             return None
         user = model.User.model_validate(resp[0])
         return user
+
 
 User = _User()
