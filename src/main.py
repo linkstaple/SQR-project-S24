02b5d822d09c01727c3c2129e357e08dc121c927
Michael Dmitriev <m.dmitriev@innopolis.university>
2024-04-26 14:39:06 +0300

    added html requests handling

--- src/main.py
+++ src/main.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static-css", StaticFiles(directory="static-css"), name="static-css")
+app.mount("/static-js", StaticFiles(directory="static-js"), name="static-js")
 
 templates = Jinja2Templates(directory="static")
+htmlTemplates = Jinja2Templates(directory="static-html")
 
 # Dependency
 def get_db():
@@ -61,6 +64,30 @@ async def group_info(id: str):
 @app.post("/api/split")
 async def group():
     return {"message": "Hello World"}
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="login.html", context={'script_type': 'login'}
+    )
+
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="login.html", context={'script_type': 'register'}
+    )
+
+@app.get("/profile", response_class=HTMLResponse)
+async def profile_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="profile.html"
+    )
+
+@app.get("/group/{group_id}")
+def update_item(request: Request, group_id: int):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="group.html", context={'group_id': group_id}
+    )
 
 @app.get("/easter_egg", response_class=HTMLResponse)
 async def read_item(request: Request):
