2fffcc0525a345ce53784eba903d6b1cccef085e
Michael Dmitriev <m.dmitriev@innopolis.university>
2024-04-26 23:32:52 +0300

    Revert "Merge branch 'dev' of github.com:linkstaple/SQR-project-S24"
    This reverts commit f440135a51206111f8a5252c311621a7b1774ebc, reversing
    changes made to 34d21cbba255aa042d7f6bc997e20d23714a9646.

--- src/db/sqlite.py
+++ src/db/sqlite.py
@@ -1,16 +1,83 @@
 import sqlite3
-from config import Config
 
-class _Database():
+from model import model
+
+
+class Database():
+
     def __init__(self):
-        self.connection = sqlite3.connect(Config.sqlite_path)
-        with open(Config.sqlite_init_script_path, 'r') as init_db:
-            self.connection.executescript(init_db.read())
+        self.connection = sqlite3.connect('lazy_split.db')
+        self.connection.execute('''
+            create table if not exists users (
+                id integer primary key,
+                username text not null unique,
+                password text not null
+                )
+        ''')
+
+        self.connection.execute('''
+            create table if not exists groups (
+                id integer primary key,
+                name text not null
+                )
+        ''')
+
+        self.connection.execute('''
+            create table if not exists groups_users (
+                group_id integer not null,
+                user_id integer not null,
+                constraint fk_users
+                    foreign key (user_id)
+                    references users(id)
+                
+                constraint fk_groups
+                    foreign key (group_id)
+                    references groups(id)
+                )
+        ''')
+
+        self.connection.execute('''
+            create table if not exists split_history (
+                id integer primary key,
+                group_id integer not null,
+                lander_id integer not null,
+                doer_id integer not null,
+                amount integer not null,
+                constraint fk_lander
+                    foreign key (lander_id)
+                    references users(id)
+                                
+                constraint fk_doer
+                    foreign key (doer_id)
+                    references users(id)
+                                
+                constraint fk_group
+                    foreign key (group_id)
+                    references groups(id)
+                )
+        ''')
+
+        self.connection.execute('''
+            create table if not exists split_members (
+                split_id integer not null,
+                member_id integer not null,
+                constraint fk_split
+                    foreign key (split_id)
+                    references split_history(id)
+                                
+                constraint fk_member
+                    foreign key (member_id)
+                    references users(id)
+                )
+        ''')
 
         self.connection.commit()
 
+    def new_db(self):
+        return self
+
     def __cursor(self):
-        return sqlite3.connect(Config.sqlite_path).cursor()
+        return sqlite3.connect('lazy_split.db').cursor()
 
     def query(self, query, *params):
         return self.__cursor().execute(query, params)
@@ -26,7 +93,35 @@ class _Database():
         cursor.connection.commit()
         return cursor
 
+    def list_users(self):
+        return self.query("select * from users").fetchall()
+
+    def register_user(self, username, hashed_password):
+        self.execute("insert into users (username, password) values ($1, $2)",
+                    username, hashed_password).fetchall()
+
+    def get_user_by_credentials(self, username, hashed_password) -> model.User:
+        resp = (self.
+                execute("select id, username from users where username = $1 and password = $2",
+                        username, hashed_password).
+                fetchall())
+        print("adljfjkhsdkf", resp)
+        if resp is None or len(resp) == 0:
+            print('ok')
+            return None
+        user = model.User.model_validate(resp[0])
+        return user
+
+    def get_user_groups(self, user_id) -> list[model.UserGroup]:
+        response = (self.
+                execute('''select groups_users.group_id as id, groups.name as name from groups_users
+                        left join groups on groups_users.group_id = groups.id
+                        where groups_users.user_id = $1''',
+                        user_id).
+                fetchall())
+        
+        groups = list(map(model.UserGroup.model_validate, response))
+        return groups
+
     def graceful_shutdown(self):
         self.connection.close()
-
-Database = _Database()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,117 @@
 import uvicorn
-import api
-import static
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from db.sqlite import Database
+from model.model import RegisterUser, LoginUser
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 app = FastAPI()
-api.setup(app)
-static.setup(app)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static-css", StaticFiles(directory="static/css"), name="static-css")
+app.mount("/static-js", StaticFiles(directory="static/js"), name="static-js")
+
+templates = Jinja2Templates(directory="static")
+htmlTemplates = Jinja2Templates(directory="static/html")
+
+
+# Dependency
+def get_db():
+    if not hasattr(get_db, "db"):
+        get_db.db = Database()
+
+    return get_db.db.new_db()
+
+
+@app.get("/api/")
+async def root(db: Database = Depends(get_db)):
+    return {"message": db.list_users()}
+
+
+@app.post("/api/register")
+async def register(user: RegisterUser, db: Database = Depends(get_db)):
+    username, password = user.username, user.password
+
+    existing_user = db.get_user_by_credentials(username, password)
+    if existing_user is not None:
+        return JSONResponse(
+            content={'message': "Username \"{username}\" is already taken"},
+            status_code=409,
+        )
+    else:
+        db.register_user(username, password)
+        new_user = db.get_user_by_credentials(username, password)
+        return JSONResponse(status_code=200, content=new_user.model_dump())
+
+
+@app.post("/api/login")
+async def login(user: LoginUser, db: Database = Depends(get_db)):
+    user = db.get_user_by_credentials(user.username, user.password)
+    if user is None:
+        resp = JSONResponse(content="user or password is incorrect")
+        resp.status_code = 404
+        return resp
+    resp = JSONResponse(content=user.model_dump())
+    return resp
+
+
+@app.get("/api/profile")
+async def profile():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/groups")
+async def groups(db: Database = Depends(get_db)):
+    user_groups = db.get_user_groups(1) #todo extract user_id from request
+    return JSONResponse(content={'groups': list(map(lambda model: model.model_dump(), user_groups))}, status_code=200)
+    return {"message": "Hello World"}
+
+
+@app.post("/api/group")
+async def group():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/group/{id}")
+async def group_info(id: str):
+    return {"message": "Hello World"}
+
+
+@app.post("/api/split")
+async def group():
+    return {"message": "Hello World"}
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="login.html", context={'script_type': 'login'}
+    )
+
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="login.html", context={'script_type': 'register'}
+    )
+
+@app.get("/profile", response_class=HTMLResponse)
+async def profile_page(request: Request):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="profile.html"
+    )
+
+@app.get("/group/{group_id}")
+def update_item(request: Request, group_id: int):
+    return htmlTemplates.TemplateResponse(
+        request=request, name="group.html", context={'group_id': group_id}
+    )
+
+@app.get("/easter_egg", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
