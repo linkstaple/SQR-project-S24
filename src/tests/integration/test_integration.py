07640f5a22a4cf991bf86bde248380634ad681b0
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2024-04-28 02:05:17 +0300

    intergration test

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -1,204 +1,222 @@
-# import dataclasses
-# import json
-# import os
-# import sys
-# import traceback
-# import typing
-# from time import sleep
+import dataclasses
+import json
+import os
+import sys
+import traceback
+import typing
+from time import sleep
+
+from httpx import AsyncClient
+
+import pytest
+from fastapi import FastAPI
+
+sys.path.append('src')
+
+from model import Group, GroupList
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+def set_up(mocker) -> FastAPI:
+    os.open('test_lazy_split.db', flags=os.O_CREAT)
+
+    sys.path.append('src/')
+
+    users_exists = mocker.patch(
+        'config.Config.sqlite_path',
+        'test_lazy_split.db',
+    )
+
+    import api
+    import middleware
+    import static
+
+    app = FastAPI()
+    middleware.setup(app)
+    api.setup(app)
+    static.setup(app)
+
+    return app
+
+
+def teardown():
+    os.remove('test_lazy_split.db')
+
+
+@dataclasses.dataclass
+class User:
+    id: int
+    username: str
+    password: str
+    token: str
+
+
+async def register_user(app: FastAPI, base_url: str, username: str, password: str) -> User:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("POST", '/api/register',
+                                    json={
+                                        "username": username,
+                                        "password": password,
+                                    })
+        assert response.status_code == 200
+
+        resp = response.json()
+        user = User(
+            username=username,
+            password=password,
+            id=resp['id'],
+            token=resp['token'],
+        )
+
+        return user
+
+
+async def get_profile(app: FastAPI, base_url: str, user: User) -> User:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("GET", '/api/profile', headers={
+            "Authorization": f"Bearer {user.token}"
+        })
+        assert response.status_code == 200
+
+        print(response.json())
+        return User(
+            username=response.json()["username"],
+            id=response.json()['id'],
+            token=response.json()['token'],
+            password='',
+        )
+
+
+async def create_group(app: FastAPI, base_url: str, user: User, group_name: str, user_ids: typing.List[int]) -> Group:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("POST", '/api/group', json={
+            "name": group_name,
+            "member_ids": user_ids,
+        }, headers={
+            "Authorization": f"Bearer {user.token}",
+        })
+        assert response.status_code == 200
+
+        return Group.model_validate(response.json())
+
+
+async def list_groups(app: FastAPI, base_url: str, user: User) -> GroupList:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("GET", '/api/groups', headers={
+            "Authorization": f"Bearer {user.token}",
+        })
+        assert response.status_code == 200
+
+        return GroupList.model_validate(response.json())
+
+
+async def get_group_info(app: FastAPI, base_url: str, user: User, group_id: int) -> Group:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("GET", f'/api/group/{group_id}', headers={
+            "Authorization": f"Bearer {user.token}",
+        })
+        assert response.status_code == 200
+
+        return Group.model_validate(response.json())
+
+
+async def split_money(app: FastAPI, base_url: str, user: User, group_id: int, amount: float,
+                      payers: typing.List[int]) -> Group:
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.request("POST", f'/api/split', json={
+            "group_id": group_id,
+            "amount": amount,
+            "lander_id": user.id,
+            "payer_ids": payers,
+
+        }, headers={
+            "Authorization": f"Bearer {user.token}",
+        })
+        assert response.status_code == 200
+
+        return Group.model_validate(response.json())
+
+
+@pytest.mark.asyncio
+async def test_api_common_operations(mocker):
+    app = set_up(mocker)
+
+    try:
+        base_url = "http://127.0.0.1:8000"
+        user1 = await register_user(app, base_url,
+                                    'timur', '123')
+        user2 = await register_user(app, base_url,
+                                    'andrey', '1234')
+        user3 = await register_user(app, base_url,
+                                    'misha', '12345')
+
+        profile_user_3 = await get_profile(app, base_url, user3)
+        assert profile_user_3.id == user3.id
+        assert profile_user_3.username == user3.username
+
+        group_1_2 = await create_group(app, base_url,
+                                       user1, "sky part", [user2.id])
+
+        group_1_3 = await create_group(app, base_url,
+                                       user1, "restaurant", [user1.id, user3.id])
+
+        group_all = await create_group(app, base_url,
+                                       user1, "party", [user2.id, user3.id])
+
+        user_3_groups = await list_groups(app, base_url, user3)
+        assert len(user_3_groups.groups) == 2
+        assert user_3_groups.groups[0].id == group_1_3.id
+        assert user_3_groups.groups[1].id == group_all.id
+        assert user_3_groups.groups[0].name == group_1_3.name
+        assert user_3_groups.groups[1].name == group_all.name
+
+        group_all_info = await get_group_info(app, base_url,
+                                              user3, group_all.id)
+        assert group_all_info == group_all
+
+        await split_money(app, base_url, user2, group_1_2.id, 100, [user1.id])
+        await split_money(app, base_url, user2, group_1_2.id, 100, [user1.id, user2.id])
+
+        await split_money(app, base_url, user1, group_all.id, 300, [user1.id, user2.id, user3.id])
+        await split_money(app, base_url, user3, group_all.id, 600, [user1.id, user2.id, user3.id])
+        await split_money(app, base_url, user3, group_all.id, 300, [user1.id, user2.id, user3.id])
+
+        group_all_info = await get_group_info(app, base_url,
+                                              user3, group_all.id)
+        assert len(group_all_info.history) == 3
+        # assert group_all_info.members
+
+        group_1_3_info = await get_group_info(app, base_url,
+                                              user3, group_1_3.id)
+        print(group_1_3_info)
+
+        group_1_2_info = await get_group_info(app, base_url,
+                                              user1, group_1_2.id)
+        print(group_1_2_info)
+
+
+
+    except Exception as e:
+        print("exception: ", e, traceback.format_exc())
+
+    teardown()
+
 #
-# from httpx import AsyncClient
 #
-# import pytest
-# from fastapi import FastAPI
+# payload = json.dumps({
+#     "username": "212344123596",
+#     "password": "123243"
+# })
 #
-# sys.path.append('src')
+# headers = {
+#     'Content-Type': 'application/json',
+#     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.Ggbl'
+#                      '-87EbMIijgSApQNRibOFm_omxUVWEWi58Mp7y5s'
+# }
 #
-# from model import Group, GroupList
+# async with AsyncClient(app=app, base_url=base_url) as ac:
+#     response = await ac.request("GET", '/api/profile', headers=headers, json=payload)
 #
-# pytest_plugins = ('pytest_asyncio',)
-#
-#
-# def set_up(mocker) -> FastAPI:
-#     os.open('test_lazy_split.db', flags=os.O_CREAT)
-#
-#     sys.path.append('src/')
-#
-#     users_exists = mocker.patch(
-#         'config.Config.sqlite_path',
-#         'test_lazy_split.db',
-#     )
-#
-#     import api
-#     import middleware
-#     import static
-#
-#     app = FastAPI()
-#     middleware.setup(app)
-#     api.setup(app)
-#     static.setup(app)
-#
-#     return app
-#
-#
-# def teardown():
-#     os.remove('test_lazy_split.db')
-#
-#
-# @dataclasses.dataclass
-# class User:
-#     id: int
-#     username: str
-#     password: str
-#     token: str
-#
-#
-# async def register_user(app: FastAPI, base_url: str, username: str, password: str) -> User:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("POST", '/api/register',
-#                                     json={
-#                                         "username": username,
-#                                         "password": password,
-#                                     })
-#         assert response.status_code == 200
-#
-#         resp = response.json()
-#         user = User(
-#             username=username,
-#             password=password,
-#             id=resp['id'],
-#             token=resp['token'],
-#         )
-#
-#         return user
-#
-#
-# async def get_profile(app: FastAPI, base_url: str, user: User) -> User:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("GET", '/api/profile', headers={
-#             "Authorization": f"Bearer {user.token}"
-#         })
-#         assert response.status_code == 200
-#
-#         print(response.json())
-#         return User(
-#             username=response.json()["username"],
-#             id=response.json()['id'],
-#             token=response.json()['token'],
-#             password='',
-#         )
-#
-#
-# async def create_group(app: FastAPI, base_url: str, user: User, group_name: str, user_ids: typing.List[int]) -> Group:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("POST", '/api/group', json={
-#             "name": group_name,
-#             "member_ids": user_ids,
-#         }, headers={
-#             "Authorization": f"Bearer {user.token}",
-#         })
-#         assert response.status_code == 200
-#
-#         return Group.model_validate(response.json())
-#
-#
-# async def list_groups(app: FastAPI, base_url: str, user: User) -> GroupList:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("GET", '/api/groups', headers={
-#             "Authorization": f"Bearer {user.token}",
-#         })
-#         assert response.status_code == 200
-#
-#         return GroupList.model_validate(response.json())
-#
-#
-# async def get_group_info(app: FastAPI, base_url: str, user: User, group_id: int) -> Group:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("GET", f'/api/group/{group_id}', headers={
-#             "Authorization": f"Bearer {user.token}",
-#         })
-#         assert response.status_code == 200
-#
-#         return Group.model_validate(response.json())
-#
-#
-# async def split_money(app: FastAPI, base_url: str, user: User, group_id: int, amount: float,
-#                       payers: typing.List[int]) -> Group:
-#     async with AsyncClient(app=app, base_url=base_url) as ac:
-#         response = await ac.request("POST", f'/api/split', json={
-#             "group_id": group_id,
-#             "amount": amount,
-#             "lander_id": int
-#             "payer_ids": list[int]
-#
-#         }, headers={
-#             "Authorization": f"Bearer {user.token}",
-#         })
-#         assert response.status_code == 200
-#
-#         return Group.model_validate(response.json())
-#
-#
-# @pytest.mark.asyncio
-# async def test_api_common_operations(mocker):
-#     app = set_up(mocker)
-#
-#     try:
-#         base_url = "http://127.0.0.1:8000"
-#         user1 = await register_user(app, base_url,
-#                                     'timur', '123')
-#         user2 = await register_user(app, base_url,
-#                                     'andrey', '1234')
-#         user3 = await register_user(app, base_url,
-#                                     'misha', '12345')
-#
-#         profile_user_3 = await get_profile(app, base_url, user3)
-#         assert profile_user_3.id == user3.id
-#         assert profile_user_3.username == user3.username
-#
-#         group_1_2 = await create_group(app, base_url,
-#                                        user1, "sky part", [user2.id])
-#
-#         group_1_3 = await create_group(app, base_url,
-#                                        user1, "restaurant", [user1.id, user3.id])
-#
-#         group_all = await create_group(app, base_url,
-#                                        user1, "party", [user2.id, user3.id])
-#
-#         user_3_groups = await list_groups(app, base_url, user3)
-#         assert len(user_3_groups.groups) == 2
-#         assert user_3_groups.groups[0].id == group_1_3.id
-#         assert user_3_groups.groups[1].id == group_all.id
-#         assert user_3_groups.groups[0].name == group_1_3.name
-#         assert user_3_groups.groups[1].name == group_all.name
-#
-#         group_all_info = await get_group_info(app, base_url,
-#                                               user3, group_all.id)
-#         assert group_all_info == group_all
-#
-#
-#
-#
-#
-#     except Exception as e:
-#         print("exception: ", e, traceback.format_exc())
-#
-#     teardown()
-#
-# #
-# #
-# # payload = json.dumps({
-# #     "username": "212344123596",
-# #     "password": "123243"
-# # })
-# #
-# # headers = {
-# #     'Content-Type': 'application/json',
-# #     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.Ggbl'
-# #                      '-87EbMIijgSApQNRibOFm_omxUVWEWi58Mp7y5s'
-# # }
-# #
-# # async with AsyncClient(app=app, base_url=base_url) as ac:
-# #     response = await ac.request("GET", '/api/profile', headers=headers, json=payload)
-# #
-# # print(response.status_code)
-# # print(response.json())
+# print(response.status_code)
+# print(response.json())
