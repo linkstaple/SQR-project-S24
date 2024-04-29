7efb50e85aa25de9d5126cf5bcdd0e104172aa29
Michael Dmitriev <m.dmitriev@innopolis.university>
2024-04-30 00:47:23 +0300

    update ui-tests bash script

--- run-ui-test.sh
+++ run-ui-test.sh
@@ -3,12 +3,15 @@ export SQLITE_PATH="$DB_PATH"
 export RELOAD_APP=False
 
 poetry run python src/main.py &
-APP_PID=$(echo $!) 
+APP_PID=$(echo $!)
+
 cd src/tests/ui
 npm ci
 npm run mocha
+
 cd ../../..
-pwd
 kill $APP_PID
 sleep 1
 rm $DB_PATH
+unset SQLITE_PATH
+unset RELOAD_APP
