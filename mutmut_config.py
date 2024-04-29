c4a6ceb6a69b6353b9d29c66553e3f0beb4ec2e8
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2024-04-29 20:08:18 +0300

    add tests for db/user

--- mutmut_config.py
+++ mutmut_config.py
@@ -8,5 +8,5 @@ def pre_mutation(context):
     if context.filename == '__init__.py':
         context.skip = True
 
-    if context.filename != 'src/db/group.py':
+    if context.filename != 'src/db/user.py':
         context.skip = True
