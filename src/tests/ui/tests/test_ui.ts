113224a27a62a41aa4d4def375a04558a5d782b8
Michael Dmitriev <m.dmitriev@innopolis.university>
2024-04-30 00:36:28 +0300

    splitted ui tests into separate sections

--- src/tests/ui/tests/test_ui.ts
+++ src/tests/ui/tests/test_ui.ts
@@ -6,7 +6,7 @@ import {
   UserPromptHandler
 } from 'selenium-webdriver/lib/capabilities'
 
-describe('UI tests', () => {
+describe('LazySplit UI tests', () => {
   let driver: WebDriver
   let builder: Builder
 
@@ -37,209 +37,227 @@ describe('UI tests', () => {
     driver.close()
   })
 
-  it('should load Login page for unauthorized user', async () => {
-    await driver.get('http://127.0.0.1:8000/')
-    const actionButton = await driver.findElement(By.id('action-button'))
+  describe('login & registration', () => {
+    it('should load Login page for unauthorized user', async () => {
+      await driver.get('http://127.0.0.1:8000/')
+      const actionButton = await driver.findElement(By.id('action-button'))
 
-    const url = await driver.getCurrentUrl()
-    assert.match(url, /\/login/, 'Expected login page to be opened')
+      const url = await driver.getCurrentUrl()
+      assert.match(url, /\/login/, 'Expected login page to be opened')
 
-    const buttonText = await actionButton.getText()
-    assert.equal(
-      buttonText,
-      'Register',
-      'Expected button text to be "Register"'
-    )
+      const buttonText = await actionButton.getText()
+      assert.equal(
+        buttonText,
+        'Register',
+        'Expected button text to be "Register"'
+      )
+    })
+
+    it('Register page should register', async () => {
+      await driver.get('http://127.0.0.1:8000/register')
+
+      const loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('michael')
+
+      const passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('1')
+
+      const submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
+
+      await driver.wait(
+        until.urlContains('profile'),
+        7000,
+        'Profile page is not opened'
+      )
+
+      const logoutButton = await driver.findElement(By.id('logout-button'))
+      await logoutButton.click()
+    })
+
+    it('check for empty login credentials', async () => {
+      await driver.get('http://127.0.0.1:8000/login')
+
+      const submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
+
+      driver.wait(until.alertIsPresent(), 4000, 'Expected alert to be shown')
+      let alert = await driver.switchTo().alert()
+      let alertText = await alert.getText()
+      assert.match(
+        alertText,
+        /login\sis\snot\sspecified/,
+        'Alert for non-specified username is not shown'
+      )
+
+      await alert.accept()
+
+      const loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('andrew')
+
+      await submitButton.click()
+
+      alert = await driver.switchTo().alert()
+      alertText = await alert.getText()
+      assert.match(
+        alertText,
+        /Password\sis\snot\sspecified/,
+        'Alert for non-specified password is not shown'
+      )
+      await alert.accept()
+
+      const passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('2')
+
+      await submitButton.click()
+
+      alert = await driver.switchTo().alert()
+      alertText = await alert.getText()
+      assert.match(
+        alertText,
+        /Invalid\susername\sor\spassword/,
+        'Alert for non-existing user is not shown'
+      )
+    })
+
+    it('should login user', async () => {
+      await driver.get('http://127.0.0.1:8000/login')
+
+      const loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('michael')
+
+      const passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('1')
+
+      const submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
+
+      await driver.wait(
+        until.urlContains('profile'),
+        7000,
+        'Profile page is not opened'
+      )
+
+      const logoutButton = await driver.findElement(By.id('logout-button'))
+      await logoutButton.click()
+    })
   })
 
-  it('Register page should register', async () => {
-    await driver.get('http://127.0.0.1:8000/register')
+  describe('profile page', () => {
+    it('should create group', async () => {
+      // register user Andrew
+      await driver.get('http://127.0.0.1:8000/register')
 
-    const loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('michael')
+      let loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('andrew')
 
-    const passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('1')
+      let passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('2')
 
-    const submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
+      let submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
 
-    await driver.wait(
-      until.urlContains('profile'),
-      7000,
-      'Profile page is not opened'
-    )
+      await driver.wait(
+        until.urlContains('profile'),
+        7000,
+        'Profile page is not opened'
+      )
 
-    const logoutButton = await driver.findElement(By.id('logout-button'))
-    await logoutButton.click()
-  })
+      // register user Timur
+      const logoutButton = await driver.findElement(By.id('logout-button'))
+      await logoutButton.click()
 
-  it('check for empty login credentials', async () => {
-    await driver.get('http://127.0.0.1:8000/login')
+      await driver.get('http://127.0.0.1:8000/register')
 
-    const submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
+      loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('timur')
 
-    driver.wait(until.alertIsPresent(), 4000, 'Expected alert to be shown')
-    let alert = await driver.switchTo().alert()
-    let alertText = await alert.getText()
-    assert.match(
-      alertText,
-      /login\sis\snot\sspecified/,
-      'Alert for non-specified username is not shown'
-    )
+      passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('3')
 
-    await alert.accept()
+      submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
 
-    const loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('andrew')
+      await driver.wait(
+        until.urlContains('profile'),
+        7000,
+        'Profile page is not opened'
+      )
 
-    await submitButton.click()
+      // creating new group
+      const checkboxes = await driver.findElements(By.css('[type=checkbox]'))
+      await Promise.all(checkboxes.map(async checkbox => checkbox.click()))
 
-    alert = await driver.switchTo().alert()
-    alertText = await alert.getText()
-    assert.match(
-      alertText,
-      /Password\sis\snot\sspecified/,
-      'Alert for non-specified password is not shown'
-    )
-    await alert.accept()
+      const groupNameInput = await driver.findElement(By.id('group-name-input'))
+      groupNameInput.sendKeys('abrakadabra')
 
-    const passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('2')
+      const createGroupButton = await driver.findElement(
+        By.id('create-group-button')
+      )
+      await createGroupButton.click()
 
-    await submitButton.click()
+      await driver.wait(
+        until.urlMatches(/group\/\d/),
+        7000,
+        'Expected URL to change to /group/:group_id'
+      )
+    })
 
-    alert = await driver.switchTo().alert()
-    alertText = await alert.getText()
-    assert.match(
-      alertText,
-      /Invalid\susername\sor\spassword/,
-      'Alert for non-existing user is not shown'
-    )
-  })
+    it('should show create group alerts', async () => {
+      // login and open profile
+      await driver.get('http://127.0.0.1:8000/login')
 
-  it('should login user', async () => {
-    await driver.get('http://127.0.0.1:8000/login')
+      const loginInput = await driver.findElement(By.id('login-input'))
+      await loginInput.sendKeys('michael')
 
-    const loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('michael')
+      const passwordInput = await driver.findElement(By.id('password-input'))
+      await passwordInput.sendKeys('1')
 
-    const passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('1')
+      const submitButton = await driver.findElement(By.id('auth-submit-button'))
+      await submitButton.click()
 
-    const submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
+      await driver.wait(
+        until.urlContains('profile'),
+        7000,
+        'Profile page is not opened'
+      )
 
-    await driver.wait(
-      until.urlContains('profile'),
-      7000,
-      'Profile page is not opened'
-    )
+      // trying to create a new group
+      const createGroupButton = await driver.findElement(
+        By.id('create-group-button')
+      )
+      await createGroupButton.click()
 
-    const logoutButton = await driver.findElement(By.id('logout-button'))
-    await logoutButton.click()
-  })
+      await driver.wait(until.alertIsPresent(), 1000, 'Alert is not shown')
+      let alert = await driver.switchTo().alert()
 
-  it('should create group', async () => {
-    // register user Andrew
-    await driver.get('http://127.0.0.1:8000/register')
+      let alertText = await alert.getText()
+      assert.match(
+        alertText,
+        /choose at least 1 member/,
+        'Alert about choosing at least 1 member is not shown'
+      )
+      alert.accept()
 
-    let loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('andrew')
+      const checkboxes = await driver.findElements(By.css('[type=checkbox]'))
+      assert.equal(
+        checkboxes.length > 0,
+        true,
+        'Expected to have at least 1 checkbox for users'
+      )
 
-    let passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('2')
+      const checkboxElem = checkboxes[0]
+      checkboxElem.click()
+      createGroupButton.click()
 
-    let submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
-
-    await driver.wait(
-      until.urlContains('profile'),
-      7000,
-      'Profile page is not opened'
-    )
-
-    // register user Timur
-    const logoutButton = await driver.findElement(By.id('logout-button'))
-    await logoutButton.click()
-
-    await driver.get('http://127.0.0.1:8000/register')
-
-    loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('timur')
-
-    passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('3')
-
-    submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
-
-    await driver.wait(
-      until.urlContains('profile'),
-      7000,
-      'Profile page is not opened'
-    )
-
-    // creating new group
-    const checkboxes = await driver.findElements(By.css('[type=checkbox]'))
-    await Promise.all(checkboxes.map(async checkbox => checkbox.click()))
-
-    const groupNameInput = await driver.findElement(By.id('group-name-input'))
-    groupNameInput.sendKeys('abrakadabra')
-
-    const createGroupButton = await driver.findElement(
-      By.id('create-group-button')
-    )
-    await createGroupButton.click()
-
-    await driver.wait(
-      until.urlMatches(/group\/\d/),
-      7000,
-      'Expected URL to change to /group/:group_id'
-    )
-  })
-
-  it('should show create group alerts', async () => {
-    // login and open profile
-    await driver.get('http://127.0.0.1:8000/login')
-    
-    const loginInput = await driver.findElement(By.id('login-input'))
-    await loginInput.sendKeys('michael')
-
-    const passwordInput = await driver.findElement(By.id('password-input'))
-    await passwordInput.sendKeys('1')
-
-    const submitButton = await driver.findElement(By.id('auth-submit-button'))
-    await submitButton.click()
-
-    await driver.wait(
-      until.urlContains('profile'),
-      7000,
-      'Profile page is not opened'
-    )
-
-    // trying to create a new group
-    const createGroupButton = await driver.findElement(By.id('create-group-button'))
-    await createGroupButton.click()
-
-    await driver.wait(until.alertIsPresent(), 1000, "Alert is not shown")
-    let alert = await driver.switchTo().alert()
-    
-    let alertText = await alert.getText()
-    assert.match(alertText, /choose at least 1 member/, "Alert about choosing at least 1 member is not shown")
-    alert.accept()
-
-    const checkboxes = await driver.findElements(By.css('[type=checkbox]'))
-    assert.equal(checkboxes.length > 0, true, "Expected to have at least 1 checkbox for users")
-
-    const checkboxElem = checkboxes[0]
-    checkboxElem.click()
-    createGroupButton.click()
-
-    await driver.wait(until.alertIsPresent(), 1000, "Alert is not shown")
-    alert = await driver.switchTo().alert()
-    alertText = await alert.getText()
-    assert.match(alertText, /Group name is not specified/, "Expected alert about empty group name")
+      await driver.wait(until.alertIsPresent(), 1000, 'Alert is not shown')
+      alert = await driver.switchTo().alert()
+      alertText = await alert.getText()
+      assert.match(
+        alertText,
+        /Group name is not specified/,
+        'Expected alert about empty group name'
+      )
+    })
   })
 })
